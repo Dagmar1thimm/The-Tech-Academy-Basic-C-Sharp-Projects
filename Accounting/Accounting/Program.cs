696265e6f4892aecb13b897fca66e1ccfa2ed18b
Dagmar Thimm <dagmar1thimm@gmail.com>
2019-09-27 06:05:27 +0200

    DailyHour drill

---
+++ Accounting/Accounting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            string slogan = Console.ReadLine();
+            Console.WriteLine("Remember, we're " + slogan + " on you");
+            Console.ReadLine();
+            //accounting shall be included in the console as slogan 
+        }
+    }
+}
---
+++ DailyReport drill/DailyReport drill/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DailyReport_drill
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Acedemy");
+        
+            Console.WriteLine("Student Daily Report");
+
+            Console.WriteLine("What course you are on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number");
+            string number = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer true or false.");
+            string needHelp = Console.ReadLine();
+            Console.WriteLine("Where there any positive experiences you'd like to share? Please give specifics.");
+            string experiences = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+
+        }
+    }
+}
