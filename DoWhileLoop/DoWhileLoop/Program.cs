819ce5e4af655ebcb14a79eb9c7d9a7943bdb7f3
Dagmar Thimm <dagmar1thimm@gmail.com>
2019-09-28 20:36:16 +0200

    DoWhileLoop drill

---
+++ DoWhileLoop/DoWhileLoop/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess my age!");
+            int age = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = age == 50;
+
+            do {
+
+                switch (age)
+                {
+                    case 45:
+                        Console.WriteLine("Your suggestion is 45. Please try it again!");
+                        Console.WriteLine("Guess my age!");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 65:
+                        Console.WriteLine("Your suggestion is 65. Please try it again!");
+                        Console.WriteLine("Guess my age!");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+
+                    case 50:
+                        Console.WriteLine("Your suggestion is 50. That's correct!");
+                        isGuessed = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Your suggestion is not correct");
+                        Console.WriteLine("Guess my age!");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+
+            }
+
+            while (!isGuessed);
+      
+            Console.Read();
+
+        }
+    }
+}
