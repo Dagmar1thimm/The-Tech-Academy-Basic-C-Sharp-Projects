6bf10e5ffc7a4a9a605101000e7775e9d49a97b0
Dagmar Thimm <dagmar1thimm@gmail.com>
2019-09-28 15:37:19 +0200

    Math2 drill

---
+++ Math2/Math2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Math2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            Console.WriteLine("Person " + 1);
+
+                Console.WriteLine("Hourly Rate?");
+                int hourRatePerson1 = 30;
+                Console.WriteLine(hourRatePerson1);
+
+                Console.WriteLine("Hours worked per week?");
+                int hourWeekPerson1 = 43;
+                Console.WriteLine(hourWeekPerson1);
+
+                Console.WriteLine("Weekly salary of Person " + 1 +":");
+                int salaryWeekPerson1 = hourRatePerson1 * hourWeekPerson1;
+                Console.WriteLine(salaryWeekPerson1);
+
+            Console.WriteLine("Person " + 2);
+
+                Console.WriteLine("Hourly Rate?");
+                int hourRatePerson2 = 33;
+                Console.WriteLine(hourRatePerson2);
+
+                Console.WriteLine("Hours worked per week?");
+                int hourWeekPerson2 = 42;
+                Console.WriteLine(hourWeekPerson2);
+
+                Console.WriteLine("Weekly salary of Person " + 2 + ":");
+                int salaryWeekPerson2 = hourRatePerson2 * hourWeekPerson2;
+                Console.WriteLine(salaryWeekPerson2);
+
+            bool TrueOrFalse = salaryWeekPerson1 > salaryWeekPerson2;
+            Console.WriteLine(TrueOrFalse);
+
+        }
+    }
+}
