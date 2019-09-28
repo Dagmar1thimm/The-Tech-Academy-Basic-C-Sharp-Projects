9ab43845620b04503a196ac4e3daab426572b2aa
Dagmar Thimm <dagmar1thimm@gmail.com>
2019-09-28 14:54:24 +0200

    Math1 drill

---
+++ Math1/Math1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Math1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 8;
+            int total = a * 50;
+            Console.WriteLine(total);
+            Console.ReadLine();
+
+            int b = 8;
+            int total1 = b + 25;
+            Console.WriteLine(total1);
+            Console.ReadLine();
+
+            int c = 80;
+            float total2 = c / 12.5f;
+            Console.WriteLine(total2);
+            Console.ReadLine();
+
+            int d = 8;
+            bool TrueOrFalse = d > 50;
+            Console.WriteLine(TrueOrFalse);
+            Console.ReadLine();
+
+            int e = 80;
+            int remainder = e % 7;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+        }
+    }
+}
