a0dbf41cfe9dbe087e715828a382a3753bc6bc76
Dagmar Thimm <dagmar1thimm@gmail.com>
2019-09-28 17:00:18 +0200

    BoolLogic drill

---
+++ BoolLogic/BoolLogic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BoolLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int Age = 17;
+            Console.WriteLine(Age);
+
+            Console.WriteLine("Have you ever had a DUI?");
+            int DUI = 0;
+            bool TrueOrFalse = DUI > 0;
+            Console.WriteLine(TrueOrFalse);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedTicket = 2;
+            Console.WriteLine(speedTicket);
+
+            Console.WriteLine("Qualified?");
+            bool isQualified = (Age > 15 && DUI == 0 && speedTicket <=3);
+            Console.WriteLine(isQualified);
+
+        }
+    }
+}
