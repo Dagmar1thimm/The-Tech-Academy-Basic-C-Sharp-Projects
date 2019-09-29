0ba92f9de4f678cddb30a196e5b21f4fa6bf6965
Dagmar Thimm <dagmar1thimm@gmail.com>
2019-09-29 08:52:27 +0200

    Stringv1 drill

---
+++ Stringv1/Stringv1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Stringv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Dagmar";
+            string timezone = "CET";
+            string action = "lives";
+
+            Console.WriteLine("Hello, " + name + " who " + action + " in the " + timezone + "?");
+
+
+            name = "Dagmar";
+            timezone = "CET";
+            action = "lives";
+            name = name.ToUpper();
+            Console.WriteLine("Hello, " + name + " who " + action + " in the " + timezone + "?");
+
+        }
+    }
+}
