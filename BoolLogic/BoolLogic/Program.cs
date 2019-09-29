93929cdbc472eae58fc4bc7bf637e50dc7fa8020
Dagmar Thimm <dagmar1thimm@gmail.com>
2019-09-29 20:41:28 +0200

    BoolLogic drill corrections

--- BoolLogic/BoolLogic/Program.cs
+++ BoolLogic/BoolLogic/Program.cs
@@ -9,19 +9,23 @@ namespace BoolLogic
             Console.WriteLine("What is your age?");
             int Age = 17;
             Console.WriteLine(Age);
+            Console.ReadLine();
 
             Console.WriteLine("Have you ever had a DUI?");
             int DUI = 0;
             bool TrueOrFalse = DUI > 0;
             Console.WriteLine(TrueOrFalse);
+            Console.ReadLine();
 
             Console.WriteLine("How many speeding tickets do you have?");
             int speedTicket = 2;
             Console.WriteLine(speedTicket);
+            Console.ReadLine();
 
             Console.WriteLine("Qualified?");
             bool isQualified = (Age > 15 && DUI == 0 && speedTicket <=3);
             Console.WriteLine(isQualified);
+            Console.ReadLine();
 
         }
     }
