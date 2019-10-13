e79aa2a4260472b959f71bdf7acb4fb2e3b4275c
Dagmar Thimm <dagmar1thimm@gmail.com>
2019-10-13 22:38:31 +0200

    BoolLogic DRILL2

--- StringBuilder DRILL/StringBuilder DRILL/Program.cs
+++ StringBuilder DRILL/StringBuilder DRILL/Program.cs
@@ -7,6 +7,22 @@ namespace StringBuilder_DRILL
     {
         static void Main(string[] args)
         {
+
+            string name = "Dagmar";
+            string timezone = "CET";
+            string action = "lives";
+
+            Console.WriteLine("Hello, " + name + " who " + action + " in the " + timezone + "?");
+            Console.ReadLine();
+
+            name = "Dagmar";
+            timezone = "CET";
+            action = "lives";
+            name = name.ToUpper();
+            action = action.ToUpper();
+            Console.WriteLine("Hello, " + name + " who " + action + " in the " + timezone + "?");
+            Console.ReadLine();
+
             StringBuilder sb = new StringBuilder("Dagmar lives in CET timezone. ");
             sb.Append("Hans, too.");
             Console.WriteLine(sb);
