1a2dbc241e15a5d169c804359fd4bd2a133be049
Dagmar Thimm <dagmar1thimm@gmail.com>
2019-10-13 16:44:33 +0200

    Branching DRILL

--- BoolLogic/BoolLogic/Program.cs
+++ BoolLogic/BoolLogic/Program.cs
@@ -9,21 +9,20 @@ namespace BoolLogic
             Console.WriteLine("What is your age?");
             int Age = 17;
             Console.WriteLine(Age);
-            Console.ReadLine();
+            int Age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Have you ever had a DUI?");
-            int DUI = 0;
-            bool TrueOrFalse = DUI > 0;
-            Console.WriteLine(TrueOrFalse);
-            Console.ReadLine();
+            Console.WriteLine("Have you ever had a DUI? true or false");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+
+            
 
             Console.WriteLine("How many speeding tickets do you have?");
             int speedTicket = 2;
             Console.WriteLine(speedTicket);
-            Console.ReadLine();
+            int speedTicket = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Qualified?");
-            bool isQualified = (Age > 15 && DUI == 0 && speedTicket <=3);
+            bool isQualified = (Age > 15 && DUI == false && speedTicket <=3);
             Console.WriteLine(isQualified);
             Console.ReadLine();
 
--- Branching/Branching/Program.cs
+++ Branching/Branching/Program.cs
@@ -9,39 +9,42 @@ namespace Branching
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
             Console.ReadLine();
 
-            Console.WriteLine("Please enter the package weight:");
-            int weight = 51;
-            if (weight > 50);
-            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
-            int weight = Convert.ToInt32(Console.ReadLine());
+            int weight = 50;
+            int sum = 50;
 
-            Console.WriteLine("Please enter the package width:");
-            int width = 10;
-            Console.WriteLine(width);
-            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package myWeight:");
+            int myWeight = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please enter the package heigth:");
-            int height = 30;
-            Console.WriteLine(height);
-            height = Convert.ToInt32(Console.ReadLine());
+            if (myWeight > weight)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
 
-            Console.WriteLine("Please enter the package length:");
-            int length = 30;
-            Console.WriteLine(length);
-            int length = Convert.ToInt32(Console.ReadLine());
+            else if (myWeight <= weight)
 
-            if (width + height + length > 50);
-            Console.WriteLine("Package too big to be shipped via Package Express.");
-            
+            {
+                Console.WriteLine("Please enter the package width:");
+                int width = Convert.ToInt32(Console.ReadLine());
 
-            int sum = (width + height + length * weight / 100);
-            int sum = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your estimated total for shipping this package is:");
-            Console.WriteLine("$" + sum);
-         
-            Console.WriteLine("Thank you");
-            
+                Console.WriteLine("Please enter the package heigth:");
+                int height = Convert.ToInt32(Console.ReadLine());
 
+                Console.WriteLine("Please enter the package length:");
+                int length = Convert.ToInt32(Console.ReadLine());
+
+                int quote = (width + height + length * myWeight / 100);
+
+                string result = width + height + length > sum ? "Package too big to be shipped via Package Express. " : "Your estimated total for shipping this package is: " + "$" + quote;
+
+                Console.WriteLine(result);
+                Console.ReadLine();
+               
+            }
+
+           Console.WriteLine("Thank you");
         }
+                   
     }
+            
 }
+ 
