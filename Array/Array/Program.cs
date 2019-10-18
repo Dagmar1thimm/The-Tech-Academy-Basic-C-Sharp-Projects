d49fff566e4166d7f249f11f1678aa817529a56c
Dagmar Thimm <dagmar1thimm@gmail.com>
2019-10-18 21:10:17 +0200

    StringBuilder DRILL2

---
+++ Array/Array/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Array
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] strArr = new string[] { "Fred", "Steve", "Carla", "Curt", "Julia" };
+            Console.WriteLine("Who is the expert for coding in your perspective?");
+
+
+            Console.WriteLine(strArr[3]);
+            Console.ReadLine();
+
+
+
+
+            int[] numArr = { 4, 3, 9, 1, 2 };
+            int i = 3;
+            Console.WriteLine("Which number do you prefer?");
+            Console.WriteLine(numArr[i]);
+            Console.ReadLine();
+
+            
+            i = 7;
+            if (i > numArr.Length)
+     
+               {
+                Console.WriteLine("Wrong entry, Please enter a lower number");
+            }
+            Console.ReadLine();
+
+            List<string> intList = new List<string>();
+            intList.Add ("Hi");
+            intList.Add("John");
+            intList.Add("Leila");
+            Console.WriteLine(intList[2]);
+            Console.ReadLine();
+        }
+
+        }
+    }
+
+    
