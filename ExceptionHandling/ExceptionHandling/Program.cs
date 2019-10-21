1f581baa31cb2fa0dfbde0aa55a50a85d531f843
Dagmar Thimm <dagmar1thimm@gmail.com>
2019-10-21 07:37:06 +0200

    Exception Handling Drill

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wholeNum = new List<int> { 75, 4, 6, 98, 30, 28 };
+            try
+            {
+                Console.WriteLine("Please enter your divider: ");
+                int divider = Convert.ToInt32(Console.ReadLine());
+
+                foreach (var item in wholeNum)
+                {
+                    var result = item / divider;
+                    Console.WriteLine(result);
+                }
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please do not divide by zero");
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a valid format");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
