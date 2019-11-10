d075fe9bc1c4774643843aa9c3d9b06b7ff184ea
Dagmar Thimm <dagmar1thimm@gmail.com>
2019-11-10 21:45:06 +0100

    First TRY

---
+++ Method/Method/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            int x = 5;
+            int y = 6;
+
+            VoidM results = new VoidM();
+          
+            Console.WriteLine("My first number: " + results.Math(x) + " My second number:  " + results.Screen(y));
+            
+          
+        }   
+
+           
+    }
+}
+
---
+++ Method/Method/VoidM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method
+{
+    class VoidM
+    {
+        public void Math(int x)
+        {
+            int results = x + 2;
+
+            return;
+        }
+
+        public void Screen(int y)
+        {
+            int results = y;
+
+            return;
+        }
+
+    }
+}
