ab7ee98bcea908b49efdd75aeec4c2b80743fc67
Dagmar Thimm <dagmar1thimm@gmail.com>
2020-02-24 22:27:38 +0100

    First attempt

---
+++ InheriInterface/InheriInterface/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheriInterface
+{
+    class Employee : Person, IQuittable
+    {
+
+        private int Id;
+        public void setEmployeeId(int eID)
+        {
+            Id = eID;
+            Console.WriteLine(eID);
+        }
+
+        public void Quit()
+        {
+            throw new NotImplementedException();
+
+        }
+
+    }
+}
---
+++ InheriInterface/InheriInterface/IQuittable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheriInterface
+{
+    interface IQuittable
+    {
+        void Quit();
+    }
+}
