7fc383b05aaab9ab8f149c8dbaaad25fbc6cd9c9
Dagmar Thimm <dagmar1thimm@gmail.com>
2020-02-26 11:30:43 +0100

    first

---
+++ EnumDay/EnumDay/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EnumDay
+{
+    class Program
+    {
+        enum WeekDays {Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday };
+
+        static void Main(string[] args)
+        {
+
+            WeekDays myDay;
+            bool noValidDay = true;
+
+            while (noValidDay)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter the day of the week:");
+                    myDay = (WeekDays)Enum.Parse(typeof(WeekDays), Console.ReadLine());
+                    noValidDay = false;
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Please enter an ACTUAL day of the week.");
+                    noValidDay = true;
+                }
+            }
+            Console.WriteLine("Have a nice day!" );
+            Console.ReadLine();
+        }
+    }
+}
